21e6a6c46fb787469f937a36fbd8e4a43bb52948
varuwa <varuwa@gmail.com>
2020-04-01 16:31:14 +0300

    added testOpenProfile

--- first-selenium-project/src/test/java/com/telran/qa/tests/OpenGoogleTest.java
+++ first-selenium-project/src/test/java/com/telran/qa/tests/OpenGoogleTest.java
@@ -2,9 +2,9 @@ package com.telran.qa.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -14,27 +14,37 @@ WebDriver wd;
 
 @BeforeClass
 public void setUp(){
-wd = new ChromeDriver();
-wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-wd.get("https://www.google.com/");
+    wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    wd.get("https://www.google.com/");
+}
+
+private void click(By locator) {
+    wd.findElement(locator).click();
 }
 
 @Test
-public void testOpenGoogle(){
-    wd.findElement(By.className("gb_D")).click();
-    wd.findElement(By.className("gb_D")).click();
+public void testOpenGoogle() throws InterruptedException {
+    click(By.className("gb_D"));
+    click(By.className("gb_D"));
+    Thread.sleep(3000);
 }
 
 @Test
-public void testSearchGoogle(){
-    wd.findElement(By.name("q")).click();
+public void testSearchGoogle() throws InterruptedException {
+    click(By.name("q"));
     wd.findElement(By.name("q")).clear();
     wd.findElement(By.name("q")).sendKeys("Java");
-    
+    Thread.sleep(3000);
 }
 
+@Test
+    public void testOpenProfile() throws InterruptedException {
+    click(By.id("gb_70"));
+    Thread.sleep(3000);
+}
 
-@AfterMethod
+@AfterClass
 public void tearDown(){
 wd.quit();
 }
