fdd63dd07a22ed2eaccd6eff3862054b174f0ae2
varuwa <varuwa@gmail.com>
2020-04-15 18:11:28 +0300

    added Team creation test

--- trello-web-tests/src/test/java/com/qa/trello/BoardCreationTests.java
+++ trello-web-tests/src/test/java/com/qa/trello/BoardCreationTests.java
@@ -3,8 +3,6 @@ package com.qa.trello;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 public class BoardCreationTests {
     WebDriver wd;
-    WebDriverWait wait; //класс ждать, если нужно подождать подгрузку на странице
+    WebDriverWait wait; //класс ждать, если нужно подождать подгрузку на странице принудительно
 
     @BeforeMethod //аннотация
     public void setUp() { //
@@ -27,20 +25,35 @@ public class BoardCreationTests {
 
     @Test
     public void testBoardCreation() {
-
         initLogin();
         fillLoginForm("varuwa@gmail.com", "trellobarbara");
         confirmLogin();
+        initBoardCreation();
+        fillBoardForm("Test", "[title='blue']");
+        confirmBoardCreation();
+        returnToHomePage();
+    }
 
-        //initBoardCreation
-        //fillBoardForm
-        //confirmBoardCreation
-        //returnToHomePage
+    private void returnToHomePage() {
+        click(By.name("house"));
+    }
 
+    private void initBoardCreation() {
+        click(By.name("add"));
+        click(By.cssSelector("[data-test-id='header-create-board-button']"));
+    }
+
+    private void confirmBoardCreation() {
+        click(By.cssSelector("[class='_3UeOvlU6B5KUnS uj9Ovoj4USRUQz _2MgouXHqRQDP_5']"));
+    }
+
+    private void fillBoardForm(String nameOfBoard, String colorOfBoard) {
+        type(By.cssSelector("[data-test-id='create-board-title-input']"), nameOfBoard);
+        click(By.cssSelector(colorOfBoard));
     }
 
     private void confirmLogin() {
-       // wait.until(ExpectedConditions.presenceOfElementLocated(By.id(""))) если элемент не подгружается
+       //wait.until(ExpectedConditions.presenceOfElementLocated(By.id("login"))); //жди жо тех пор пока элемент не появится
         click(By.id("login"));
     }
 
@@ -65,8 +78,6 @@ public class BoardCreationTests {
 
     @AfterMethod
     public void tearDown() { //закрывает тест
-        // wd.quit(); //закрыть браузер
+        wd.quit(); //закрыть браузер
     }
-
-
 }
